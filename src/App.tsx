ba4b058dc240a02c668f4620b1145ede4d88ffc5
Matheus Roichman <roichmankill@gmail.com>
2023-01-20 01:49:49 -0300

    finished project functions; TODO: layout with CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import {
+  StorageError,
+  StorageReference,
+  UploadTask,
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { FormEvent, useState } from "react";
+import { storage } from "./firebase/firebaseConfig";
 
 function App() {
+  const [file, setFile] = useState<File | null>(null);
+  const [localUrl, setLocalUrl] = useState<string | null>(null);
+  const [firebaseUrl, setFirebaseUrl] = useState<string | null>(null);
+  const [progress, setProgress] = useState<number>(0);
+  const [error, setError] = useState<string | null>(null);
+
+  const onChange = (event: FormEvent<HTMLInputElement>) => {
+    if (!event.currentTarget.files) return;
+
+    const file = event.currentTarget.files[0];
+
+    setFile(file);
+    setLocalUrl(URL.createObjectURL(file));
+  };
+
+  const handleUpload = (file: File): void => {
+    const storageRef: StorageReference = ref(
+      storage,
+      `images/${file.name}-${Date.now()}`
+    );
+    const uploadTask: UploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed", 
+      (snapshot) => {
+        const progress = parseInt(((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toString());
+        setProgress(progress);
+      },
+      (error) => {
+        setError('Erro ao fazer o upload da imagem. Tente novamente.')
+      },
+      async () => {
+        try {
+          const url: string = await getDownloadURL(uploadTask.snapshot.ref);
+          setFirebaseUrl(url);
+        } catch (error) {
+          setError('Rrro ao resgatar a URL da imagem.')
+        }
+      }
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <h1>Upload de imagens</h1>
+      <input type="file" accept="image/*" onChange={onChange} />
+      {localUrl ? (
+        <>
+          <p>IMAGEM LOCAL</p>
+          <img src={localUrl} alt={localUrl} width="300" height="300" />
+        </>
+      ) : null}
+      <button
+        onClick={() => {
+          if (!file) return;
+
+          return handleUpload(file);
+        }}
+      >
+        Upload
+      </button>
+      <p>Progresso: {progress}%</p>
+      {error ? <p>{error}</p> : null}
+      {firebaseUrl ? (
+        <>
+          <p>IMAGEM EM NUVEM</p>
+          <img src={firebaseUrl} alt={firebaseUrl} width="300" height="300" />
+        </>
+      ) : null}
+    </>
   );
 }
 
